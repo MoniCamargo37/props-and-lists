0db9755e0ae366b54cf7c0bde688c514ca170f7e
MoniCamargo37 <moni28ac@gmail.com>
2023-02-27 14:54:58 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import animalData from './data.json';
 import Card from './components/Card';
 
-function App() {
+function App() { 
+  const [animals, setAnimals] = useState(animalData);
+
+  const handleSortByLife = () => {
+    const sortedAnimals = [...animals].sort((a, b) => a.lifeExpectancy - b.lifeExpectancy);
+    setAnimals(sortedAnimals);
+  };
+
+  const handleFilterDogs = () => {
+    const dogAnimals = animals.filter(elem => elem.type === 'dog');
+    setAnimals(dogAnimals);
+  };
 
   return (
     <div className="App">
       <h1>Adopt me plz 🐾</h1>
-      {/* Some code goes here */}
+      <div className="action_container">
+        <button className="action-btn" onClick={handleSortByLife}>Sort by life</button>
+        <button className="action-btn" onClick={handleFilterDogs}>Show only Dog </button>
+      {animals.map(elem => {
+        return <Card key={elem._id} animal={elem}/>
+      })}
+    </div>
     </div>
   );
 }
